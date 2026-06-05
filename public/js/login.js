cb60130f354aa4808ea7a60df2a08b9a31cce669
Davi Gusmao Vinciaqui <davigusmaovinciaqui9040@gmail.com>
2026-06-05 15:51:13 -0300

    foi refatorado as pastas e organizado o site

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,26 @@
 // login.js
+const el ={
+    inputNome: document.getElementById("nomeCompleto"),
+    inputEmail: document.getElementById("emailCadastro"),
+    inputTelefone: document.getElementById("telefoneCadastro"),
+    inputSenha: document.getElementById("senhaCadastro")
+}
+function maskEmail(emailCadastro) {
+  // Regex para capturar: 1ª letra, restante do nome, o @, e o domínio
+  return emailCadastro.replace(/^(.)(.*)(.@.*)$/, (_, a, b, c) => {
+    return a + b.replace(/./g, '*') + c;
+  });
+}
+
+const original = "usuarioexemplo@dominio.com";
+const masked = maskEmail(original); 
+// Resultado: u***********o@dominio.com
+
+
+
+
+
+
 export function realizarLogin(usuario, senha) {
     try {
         if (!usuario || !senha) {
